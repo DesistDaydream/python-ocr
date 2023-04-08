2eb8cd29325b6d07565542995d56fdbce78aadd0
LiChenhao <39847025+DesistDaydream@users.noreply.github.com>
2023-04-08 15:05:07 +0800

    upgrade

--- cmd/dtcg_capture.py
+++ cmd/dtcg_capture.py
@@ -1,36 +1,55 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 from paddleocr import PaddleOCR, paddleocr
 import cv2
 import msvcrt
 
-paddleocr.logging.disable()
 
-cap = cv2.VideoCapture(0)
-ocr = PaddleOCR(use_angle_cls=True, lang="ch")
+class CardQuantity:
+    def __init__(self, serial, quantity):
+        self.serial = serial
+        self.quantity = quantity
 
-while True:
-    if msvcrt.kbhit():  # 检测是否有按键输入
-        key = msvcrt.getch()
-        if key == b"q":  # 如果输入 'q'，退出程序
-            break
-        else:
-            retval, frame = cap.read()  # 此刻拍照
-            if not retval:
-                print("无法接收到帧，请重试")
 
-            if key == b"s":
-                file = "imgs/example.png"
-                cv2.imwrite(file, frame)
-                img = cv2.imread(file)
-                # cardSerial = img[450:464, 335:398]
+if __name__ == "__main__":
+    paddleocr.logging.disable()
 
-            cardSerialResult = ocr.ocr(frame, cls=True)
-            for idx in range(len(cardSerialResult)):
-                res = cardSerialResult[idx]
-                for line in res:
-                    print(line)
-                    # q: 如果 if line[1][0] 中包含 EX 字符串，就打印出来
-                    if "EX" in line[1][0]:
-                        print(line[1][0])
-cap.release()
+    cap = cv2.VideoCapture(0)
+    ocr = PaddleOCR(use_angle_cls=True, lang="ch")
+    card_quantities = {}
+
+    while True:
+        if msvcrt.kbhit():  # 检测是否有按键输入
+            key = msvcrt.getch()
+            if key == b"q":  # 如果输入 'q'，退出程序
+                break
+            else:
+                retval, frame = cap.read()  # 此刻拍照
+                if not retval:
+                    print("无法接收到帧，请重试")
+
+                if key == b"s":
+                    file = "imgs/example.png"
+                    cv2.imwrite(file, frame)
+                    img = cv2.imread(file)
+                    # cardSerial = img[450:464, 335:398]
+
+                ocrResults = ocr.ocr(frame, cls=True)
+                for idx in range(len(ocrResults)):
+                    ocrResultLines = ocrResults[idx]
+                    for line in ocrResultLines:
+                        if "EX" in line[1][0]:
+                            cardSerial: str = line[1][0]
+                            cardSerial = cardSerial[:7]
+                            print("已识别卡牌:", cardSerial)
+                            if cardSerial in card_quantities:  # 如果已经有过记录，则将数量加1
+                                card_quantities[cardSerial].quantity += 1
+                            else:
+                                cq = CardQuantity(
+                                    cardSerial, 1
+                                )  # 如果是新的卡牌，创建一个 CardQuantity 对象
+                                card_quantities[cardSerial] = cq
+                            print("当前已添加卡牌信息:")
+                            for key in card_quantities:
+                                cq = card_quantities[key]
+                                print("卡牌序列号: ", cq.serial, ", 数量: ", cq.quantity)
+
+    cap.release()
--- pkg/paddle_ocr/hello_world.py
+++ pkg/paddle_ocr/hello_world.py
@@ -5,22 +5,29 @@ from paddleocr import PaddleOCR, draw_ocr
 ocr = PaddleOCR(
     use_angle_cls=True, lang="ch"
 )  # need to run only once to download and load model into memory
-img_path = "./imgs/messi5_new.jpg"
-result = ocr.ocr(img_path, cls=True)
-for idx in range(len(result)):
-    res = result[idx]
+img_path = "./imgs/example.png"
+ocrResult = ocr.ocr(img_path, cls=True)
+# ocrResult 是一个 list 类型
+# 其中每个元素是一个 list
+# list 中的每个元素是一个 tuple
+# tuple 中
+# - 第一个元素是一个 list，表示识别到的文本框的四个顶点坐标
+# - 第二个元素是一个 tuple，表示识别到的文本框中的文本和可信度
+for idx in range(len(ocrResult)):
+    res = ocrResult[idx]
     for line in res:
-        print(line)
+        print("这是识别到的文本框的四个顶点坐标: ", type(line[0]), line[0])
+        print("这是识别到的文本框中的文本和可信度: ", type(line[1]), line[1])
 
 # 显示结果
 # 如果本地没有simfang.ttf，可以在doc/fonts目录下下载
 from PIL import Image
 
-result = result[0]
+ocrResult = ocrResult[0]
 image = Image.open(img_path).convert("RGB")
-boxes = [line[0] for line in result]
-txts = [line[1][0] for line in result]
-scores = [line[1][1] for line in result]
+boxes = [line[0] for line in ocrResult]
+txts = [line[1][0] for line in ocrResult]
+scores = [line[1][1] for line in ocrResult]
 im_show = draw_ocr(image, boxes, txts, scores, font_path="doc/fonts/simfang.ttf")
 im_show = Image.fromarray(im_show)
 im_show.save("result.jpg")
